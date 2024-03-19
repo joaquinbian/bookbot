c86a5584d91219dfecbfcabff2f96434f5106a79
joaquinbian <joacobian21@gmail.com>
2024-03-19 01:53:35 -0300

    count words

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ print("hello world")
 
 
 def main():
-    with open("books/frankenstein.txt") as file:
-        res = file.read()
-        print(res)
+    frankensteinPath = "books/frankenstein.txt"
+    res = readBook(frankensteinPath)
+    wordsCount = countWords(res)
+    print("Frankenstein has ", wordsCount," words")
+    
+
+
+def readBook(path):
+        with open(path) as file:
+            res = file.read()
+            return res
+
+
+def countWords(text = " "):
+    textArr = text.split()
+    textLen = len(textArr)
+    return textLen
 
 
 main()
